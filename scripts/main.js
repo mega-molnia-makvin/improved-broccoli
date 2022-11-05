89752091dc4f2784b13021436362c6a2af89679e
mega-molnia-makvin <alinasilakova12322@gmail.com>
2022-11-05 21:14:52 +0300

    3 commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,19 @@
+//кнопочка для призыва котика
 const butt=document.querySelector('#button');
 const img=document.querySelector('#img');
 
 butt.addEventListener("click", function() {
-img.classList.toggle('content-hidden');
+if (img.classList.toggle('content-hidden')){
+    butt.textContent= 'push for a cat';
+}
+else {butt.textContent= 'push for not cat';}
 });
+
+//баян
+const headers =document.querySelectorAll('[data-name="bayan"]')
+headers.forEach(function(item){
+    item.addEventListener("click",function(){
+        this.nextElementSibling.classList.toggle('content-hidden');
+
+    })
+})
