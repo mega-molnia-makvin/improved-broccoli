eb7af0d214ee0f73ce050f67527754541e77c5ec
mega-molnia-makvin <alinasilakova12322@gmail.com>
2022-11-06 00:34:24 +0300

    3 commit

--- scripts/main.js
+++ scripts/main.js
@@ -11,9 +11,23 @@ else {butt.textContent= 'push for not cat';}
 
 //баян
 const headers =document.querySelectorAll('[data-name="bayan"]')
+
 headers.forEach(function(item){
     item.addEventListener("click",function(){
         this.nextElementSibling.classList.toggle('content-hidden');
 
     })
+})
+
+//окошечко
+const buttonOpen=document.querySelector('[data-modal_butt="modal1"]');
+const modal=document.querySelector('[data-modal="modal"]');
+const buttonClose=document.querySelector('[data-modal_butt="modal2"]');
+
+buttonOpen.addEventListener("click",function(){
+    modal.classList.remove('content-hidden')
+})
+
+buttonClose.addEventListener("click",function(){
+    modal.classList.add('content-hidden')
 })
